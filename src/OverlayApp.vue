d2c4a005ed31030a14e192fa962777c7e9b20699
HSolaire <2876798934@qq.com>
2026-07-12 12:53:56 +0800

    feat: create overlay Vue entry with LyricBar only

---
+++ src/OverlayApp.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AppToast from '@/components/AppToast.vue'
+import LyricBar from '@/components/lyric/LyricBar.vue'
+</script>
+
+<template>
+  <LyricBar />
+  <AppToast />
+</template>
---
+++ src/overlay.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import './styles/global.css'
+import OverlayApp from './OverlayApp.vue'
+
+createApp(OverlayApp).mount('#app')
